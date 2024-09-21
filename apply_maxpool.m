cf9d2d05f506940f7a0e8013b8cbd29826e620e1
Deo Sinaga <amadeo.sinaga@gmail.com>
2024-09-21 16:51:02 -0400

    completed maxpool

--- apply_maxpool.m
+++ apply_maxpool.m
@@ -1,9 +1,18 @@
-function [outarray] = apply_maxpool(inarray)
-inarray = double(inarray)
-outarray = size(inarray,1)/2 , size(inarray,2)/2, size(inarray,3)
-outarray = zeros(outarray)
+function outarray = apply_maxpool(x)
+    x = double(x);
 
-for i = 1:outarray()
+    size_x = size(x, 1)/2;
+    size_y = size(x, 2)/2;
+    num_channels = size(x, 3);
+    
+    outarray = zeros(size_x, size_y, num_channels);
 
-
+    for i=1:num_channels
+        for j=1:size_x
+            for k=1:size_y
+                block = x((2*j-1):(2*j), (2*k-1):(2*k), i);
+                outarray(j,k,i)=max(block(:));
+            end
+        end
+    end
 end
