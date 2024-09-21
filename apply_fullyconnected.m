117eca438165c5385175eda46aebfed80b64488f
Daniel Gao <123219743+danielgao-1@users.noreply.github.com>
2024-09-21 16:47:39 -0400

    function fully_connected (unfinished file)

---
+++ apply_fullyconnected.m
@@ -0,0 +1,7 @@
+function [outputArg1,outputArg2] = apply_fullyconnected(inputArg1,inputArg2)
+%APPLY_FULLYCONNECTED Summary of this function goes here
+%   Detailed explanation goes here
+outputArg1 = inputArg1;
+outputArg2 = inputArg2;
+end
+
